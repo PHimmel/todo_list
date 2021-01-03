a55cd6d7a6acf3d0a628d54977cfc3529d20d88c
PHimmel <peterhimmelman@yahoo.com>
2021-01-03 16:23:02 -0500

    Test, from PyCharm.

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from lib import todo
-
-# from lib import gui
-# tkinter version not compatible with mac big sur os
-
-def create_todo_list(ls):
-    to_do = todo.Todo()
-    [to_do.set_items(n) for n in ls]
-    return to_do
-
-def make_list():
-    total = []
-    while True:
-        ls = input('What do you need to do? Key `Enter` when done.\n')
-        if ls == '':
-            break
-        total.append(ls)
-    return total
-
-def what_has_been_done():
-    pass
-
-def what_to_add():
-    pass
+# from lib import todo
+#
+# # from lib import gui
+# # tkinter version not compatible with mac big sur os
+#
+# def create_todo_list(ls):
+#     to_do = todo.Todo()
+#     [to_do.set_items(n) for n in ls]
+#     return to_do
+#
+# def make_list():
+#     total = []
+#     while True:
+#         ls = input('What do you need to do? Key `Enter` when done.\n')
+#         if ls == '':
+#             break
+#         total.append(ls)
+#     return total
+#
+# def what_has_been_done():
+#     pass
+#
+# def what_to_add():
+#     pass
 
 def main():
     # ls = ['take a walk', 'feed the dog', 'read books']
