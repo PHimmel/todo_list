4decfd82ae77f8a42623f1e04c5e446291c3bbe2
PHimmel <peterhimmelman@yahoo.com>
2021-01-03 23:44:21 -0500

    Update.

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-# from lib import todo
-#
-# # from lib import gui
-# # tkinter version not compatible with mac big sur os
-#
-# def create_todo_list(ls):
-#     to_do = todo.Todo()
-#     [to_do.set_items(n) for n in ls]
-#     return to_do
-#
-# def make_list():
-#     total = []
-#     while True:
-#         ls = input('What do you need to do? Key `Enter` when done.\n')
-#         if ls == '':
-#             break
-#         total.append(ls)
-#     return total
-#
-# def what_has_been_done():
-#     pass
-#
-# def what_to_add():
-#     pass
+from lib import todo_interface as td
+
+# ls = ['take a walk', 'feed the dog', 'read books']
+
 
 def main():
-    # ls = ['take a walk', 'feed the dog', 'read books']
-    ls = make_list() 
-    to_do = create_todo_list(ls)
+    todo = td.TodoInterface()
+
+    ls = todo.make_list()
+    to_do = todo.create_todo_list(ls)
+
     print(f'{to_do.get_items} are your items')
+
 
 if __name__ == '__main__':
     main()
