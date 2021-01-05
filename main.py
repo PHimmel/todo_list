d040e2c1d5cd5a87bf2d0fac7246cd7675eafb07
PHimmel <peterhimmelman@yahoo.com>
2021-01-05 15:01:55 -0500

    wip

--- main.py
+++ main.py
@@ -14,13 +14,6 @@ def main():
     td.update_todo(when, item)
     print(td.get_todo())
 
-    # todo = td.TodoInterface()
-    #
-    # todo.create_list_from_user_input()
-    # # t  o_do = todo.create_todo_list(ls)
-    # #todo.todo.remove_items('df')
-    # print(f'{todo.get_items()} are your items')
-
 
 if __name__ == '__main__':
     main()
