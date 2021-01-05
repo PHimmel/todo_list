dd7be48d85c36e80e654dc669e7444f102cd2be8
PHimmel <peterhimmelman@yahoo.com>
2021-01-05 01:05:51 -0500

    wip

--- lib/todo_dict.py
+++ lib/todo_dict.py
@@ -7,20 +7,31 @@ todo = {'morning': [],
         'afternoon': [],
         'evening': []}
 
+
 class Todo:
     def __init__(self):
         self.items = {}
 
-# Key value
-def prompt_for_time_of_item():
-    when = int(input('When is the item for:\n1) morning\n2) afternoon\n3) evening\n'))
-    return when
 
+class Prompt:
+    def __init__(self):
+        self.when = None
+        self.what = None
 
-# Value value
-def prompt_for_value_of_item():
-    item = str(input('What would you like to do then?\n'))
-    return item
+    # Key value
+    def prompt_for_time_of_item(self):
+        self.when = int(input('When is the item for:\n1) morning\n2) afternoon\n3) evening\n'))
+        return self.when
+
+    # Value value
+    def prompt_for_value_of_item(self):
+        self.what = str(input('What would you like to do then?\n'))
+        return self.what
+
+    @staticmethod
+    def prompt_to_continue():
+        go_on = input('Enter \'y\' to add more:\n')
+        return go_on
 
 
 def verify_time_of_item(when):
@@ -43,10 +54,12 @@ def get_todo():
 
 
 def main():
-    when = prompt_for_time_of_item()
+    prompt = Prompt()
+
+    when = prompt.prompt_for_time_of_item()
     when = verify_time_of_item(when)
 
-    item = prompt_for_value_of_item()
+    item = prompt.prompt_for_value_of_item()
     update_todo(when, item)
     print(get_todo())
 
