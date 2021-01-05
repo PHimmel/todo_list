352d52aeafc4c4ec0c86f78d7cc37cab62437fc7
PHimmel <peterhimmelman@yahoo.com>
2021-01-05 02:13:44 -0500

    migrating entry code to main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from lib import todo_interface as td
+#from lib import todo_interface as td
+from lib import todo_dict as td
 
 # ls = ['take a walk', 'feed the dog', 'read books']
 
 
 def main():
-    todo = td.TodoInterface()
+    prompt = td.Prompt()
 
-    todo.create_list_from_user_input()
-    # t  o_do = todo.create_todo_list(ls)
-    #todo.todo.remove_items('df')
-    print(f'{todo.get_items()} are your items')
+    prompt.prompt_for_time_of_item()
+    when = td.verify_time_of_item(prompt.when)
+
+    item = prompt.prompt_for_value_of_item()
+    td.update_todo(when, item)
+    print(td.get_todo())
+
+    # todo = td.TodoInterface()
+    #
+    # todo.create_list_from_user_input()
+    # # t  o_do = todo.create_todo_list(ls)
+    # #todo.todo.remove_items('df')
+    # print(f'{todo.get_items()} are your items')
 
 
 if __name__ == '__main__':
